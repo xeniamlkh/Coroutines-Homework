bce52568349482154f5b97592b435cb519ef5bd9
xenia <xenia@test.ru>
2026-06-13 13:27:43 +0300

    Coroutines Homework. Задание 1: Перейти с коллбеков на саспенд функции и корутины

--- app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
+++ app/src/main/java/otus/homework/coroutines/CatsPresenter.kt
@@ -1,8 +1,12 @@
 package otus.homework.coroutines
 
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import java.net.SocketTimeoutException
+import kotlin.coroutines.CoroutineContext
 
 class CatsPresenter(
     private val catsService: CatsService
@@ -10,19 +14,21 @@ class CatsPresenter(
 
     private var _catsView: ICatsView? = null
 
+    private val presenterScope = PresenterScope()
+
     fun onInitComplete() {
-        catsService.getCatFact().enqueue(object : Callback<Fact> {
 
-            override fun onResponse(call: Call<Fact>, response: Response<Fact>) {
-                if (response.isSuccessful && response.body() != null) {
-                    _catsView?.populate(response.body()!!)
-                }
+        presenterScope.launch {
+            try {
+                val fact = catsService.getCatFact()
+                _catsView?.populate(fact)
+            } catch (_: SocketTimeoutException) {
+                _catsView?.showToast("Не удалось получить ответ от сервера")
+            } catch (e: Exception) {
+                CrashMonitor.trackWarning(e)
+                _catsView?.showToast(e.message ?: "")
             }
-
-            override fun onFailure(call: Call<Fact>, t: Throwable) {
-                CrashMonitor.trackWarning()
-            }
-        })
+        }
     }
 
     fun attachView(catsView: ICatsView) {
@@ -31,5 +37,20 @@ class CatsPresenter(
 
     fun detachView() {
         _catsView = null
+    }
+
+    fun cancelJob() {
+        presenterScope.cancel()
+    }
+}
+
+class PresenterScope() : CoroutineScope {
+    private val job = Job()
+
+    override val coroutineContext: CoroutineContext =
+        Dispatchers.Main + job + CoroutineName("CatsCoroutine")
+
+    fun cancel() {
+        job.cancel()
     }
 }
--- app/src/main/java/otus/homework/coroutines/CrashMonitor.kt
+++ app/src/main/java/otus/homework/coroutines/CrashMonitor.kt
@@ -5,6 +5,7 @@ object CrashMonitor {
     /**
      * Pretend this is Crashlytics/AppCenter
      */
-    fun trackWarning() {
+    fun trackWarning(e: Exception) {
+        //Firebase.crashlytics.log("e")
     }
 }
